e6293dfeb9c3122d601fe51b440d4cf33193bebb
Savarte <proboiza@gmail.com>
2021-01-12 08:39:31 +0000

    работа в docker и микросервисы

---
+++ asavinykh/multiply/as-multiply.py
@@ -0,0 +1,23 @@
+import socket
+
+s = socket.socket()
+s.bind(('0.0.0.0', 9500))
+s.listen(1)
+
+conn,addr = s.accept()
+
+print('Connected: {}'.format(addr))
+
+while 1:
+    data = conn.recv(1024)
+    data.decode()
+    a = data.decode()
+    if data:
+        x = a.split(" ")
+        res = int(x[0]) * int(x[1])
+        conn.send(str(res).encode())
+        x = []
+    else:
+        print("Connection closed")
+        break
+s.close()
